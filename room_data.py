3eb707161797c0997dc3989b9cb0cb6df986ea4b
bombanira <bombanira@yahoo.co.jp>
2019-09-09 16:14:03 +0900

    change to not logging in room_data.py

--- room_data.py
+++ room_data.py
@@ -84,28 +84,41 @@ if __name__ == "__main__":
 
     previous_minute = datetime.datetime.now().minute
     while True:
-        try:
-            while True:
-                if previous_minute != datetime.datetime.now().minute:
-                    print("now is {0}".format(datetime.datetime.now()))
-                    now = datetime.datetime.now()
-                    previous_minute = now.minute
-                    room_data = RoomData(now)
-                    room_data.measure_data()
-                    weather_data = weather.Weather_api_data()
-                    weather_data.get_weather_data()
-                    outside = weather_data.read_main_data()
-                    outside.update({"weather":weather_data.read_weather_data()})
-                    room_data.send_data_make(outside)
-                    room_data.data_send_as_json()
-        except:
+        if previous_minute != datetime.datetime.now().minute:
+            print("now is {0}".format(datetime.datetime.now()))
+            now = datetime.datetime.now()
+            previous_minute = now.minute
+            room_data = RoomData(now)
+            room_data.measure_data()
+            weather_data = weather.Weather_api_data()
+            weather_data.get_weather_data()
+            outside = weather_data.read_main_data()
+            outside.update({"weather":weather_data.read_weather_data()})
+            room_data.send_data_make(outside)
+            room_data.data_send_as_json()
+    # while True:
+    #     try:
+    #         while True:
+    #             if previous_minute != datetime.datetime.now().minute:
+    #                 print("now is {0}".format(datetime.datetime.now()))
+    #                 now = datetime.datetime.now()
+    #                 previous_minute = now.minute
+    #                 room_data = RoomData(now)
+    #                 room_data.measure_data()
+    #                 weather_data = weather.Weather_api_data()
+    #                 weather_data.get_weather_data()
+    #                 outside = weather_data.read_main_data()
+    #                 outside.update({"weather":weather_data.read_weather_data()})
+    #                 room_data.send_data_make(outside)
+    #                 room_data.data_send_as_json()
+    #     except:
 
-            log_dir = 'log'
-            if os.path.isdir(log_dir):
-                pass
-            else:
-                os.makedirs(log_dir)           
-            logging.error(traceback.format_exc())
+    #         log_dir = 'log'
+    #         if os.path.isdir(log_dir):
+    #             pass
+    #         else:
+    #             os.makedirs(log_dir)           
+    #         logging.error(traceback.format_exc())
             # sys.exit()
 
     
